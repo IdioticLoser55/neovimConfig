4fe0580a2f588459fa929c36c271726857da7083
IdioticLoser55 <idioticloser55@gmail.com>
2021-10-08 19:21:28 +0100

    add treesitter. Seems to help with syntax highlighting but need to look into it more.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,11 @@ return require('packer').startup(function()
     -- Allows packer to update itself.
     use 'wbthomason/packer.nvim'
 
+    --adds plugin treesitter playground
+    use 
+    {
+        'nvim-treesitter/playground', 
+    }
     --adds plugin treesitter
     use 
     {
