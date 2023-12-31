36d79194990f20d5b3f7f776ea96abfc9db8da69
Idiot_icLoser55 <IdioticLoser55@gmail.com>
2023-12-31 17:58:49 +0000

    added rainbow brackets, etc.

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,9 @@
+return {
+    "HiPhish/rainbow-delimiters.nvim",
+    dependencies = {
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+        require 'rainbow-delimiters'
+    end
+}
